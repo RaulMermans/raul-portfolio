728bad5c46a9fdac08a340a5cc2fb902e2695677
RaulMermans <raulmermans@gmail.com>
2026-03-08 09:22:02 +0100

    fix: remove film strip and fix text truncation on case study pages

--- components/FilmStrip.tsx
+++ components/FilmStrip.tsx
@@ -1,18 +1,5 @@
 'use client'
 
 export default function FilmStrip() {
-  return (
-    <>
-      <div className="film-strip film-strip--left" aria-hidden="true">
-        {[...Array(7)].map((_, i) => (
-          <div key={i} className="film-strip__sprocket" />
-        ))}
-      </div>
-      <div className="film-strip film-strip--right" aria-hidden="true">
-        {[...Array(7)].map((_, i) => (
-          <div key={i} className="film-strip__sprocket" />
-        ))}
-      </div>
-    </>
-  )
+  return null
 }
